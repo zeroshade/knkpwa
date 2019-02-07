eec8224ce9d163e0d1cc81566eb98ca237e5a0f0
Matthew Topol <mtopol@factset.com>
2019-02-06 19:39:30 -0500

    Updating the icons and manifest

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,42 @@
+var WebpackPwaManifest = require('webpack-pwa-manifest');
+var path = require('path');
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new WebpackPwaManifest({
+        name: 'KnK Schedule',
+        fingerprints: false,
+        inject: true,
+        short_name: 'KnK',
+        background_color: '#0E8DF1',
+        themeColor: '#F68043',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('src/assets/logo.png'),
+            sizes: [128, 256, 384],
+            destination: path.join('img', 'icons')
+          }
+        ]
+      })
+    ]
+  },
   pwa: {
     workboxPluginMode: 'InjectManifest',
-    name: 'KnK Schedule',
+    name: 'KnK Sched',
     themeColor: '#F68043',
-    assetsVersion: '1',
+    msTileColor: '#0E8DF1',
+    appleMobileWebAppCapable: 'yes',
+    assetsVersion: '2',
+    manifestPath: 'manifest.json',
     workboxOptions: {
       swSrc: 'src/service-worker.js',
       swDest: 'service-worker.js'
+    },
+    iconPaths: {
+      appleTouchIcon: 'img/icons/apple-icon-152x152.png',
+      msTileImage: 'img/icons/ms-icon-144x144.png'
     }
   }
 }
