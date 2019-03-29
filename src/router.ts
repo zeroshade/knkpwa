817cec869499689b7dc8c5f16daab74f3b70f6b3
Matthew Topol <mtopol@factset.com>
2019-03-29 11:11:20 -0400

    have title update based on vue router

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ Vue.use(VueRouter);
 
 const props = (route: Route) => ({ id: +route.params.id || 5 });
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -13,20 +13,38 @@ export default new VueRouter({
       path: '/rooms/:id?', name: 'rooms',
       props,
       component: () => import(/* webpackChunkName: "group-app" */ '@/views/RoomGrid.vue'),
+      meta: {
+        title: 'Room View',
+      },
     },
     {
       path: '/agenda/:id?', name: 'agenda',
       props,
       component: () => import(/* webpackChunkName: "group-app" */ '@/views/Agenda.vue'),
+      meta: {
+        title: 'Full View',
+      },
     },
     {
       path: '/events/:id?', name: 'events',
       props,
       component: () => import(/* webpackChunkName: "group-app" */ '@/views/Events.vue'),
+      meta: {
+        title: 'Event View',
+      },
     },
     {
       path: '/callback', component: () => import(/* webpackChunkName: "group-auth" */ '@/views/Auth.vue'),
       name: 'auth',
+      meta: {
+        title: 'Login Callback',
+      },
     },
   ],
 });
+
+router.afterEach((to, from) => {
+  document.title = 'KnK Schedule - ' + to.meta.title || 'Home';
+});
+
+export default router;
